7b6b84c99648e25106cf4fe9bc8f508c7f9d0e02
Travis Exline <t.exline22@gmail.com>
2019-05-17 15:31:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
 
-  def cobble(condition)
-    @condition = condition
+  def cobble
+    @condition
     puts "Your shoe is as good as new!"
   end
 end
